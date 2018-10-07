74df260bd81ca3dd07f399e43b686cd85f2a017e
Hudson <dhudsmith@gmail.com>
2018-10-07 07:17:40 -0400

    Refactored files and app name to be more descriptive of project

--- setup.py
+++ setup.py
@@ -15,10 +15,10 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
-    name='python-hello-world-flask',
+    name='qual-analysis-football-demo',
     version='1.0.0',
-    description='Hello World app for running Python apps on Bluemix',
+    description='An app to demostrate AI techniques for qualitative analysis using Twitter data about Clemson Football',
     long_description=long_description,
-    url='https://github.com/IBM-Bluemix/python-hello-world-flask',
+    url='https://github.com/dhudsmith/WatsonWattDashDemo',
     license='Apache-2.0'
 )
